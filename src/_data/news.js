5b4197552d3b9d2f88b138dad31085213ebb5be1
Sonam Choejur Tshering <choejur@Daemon-Hunter.local>
2020-05-19 18:39:47 +0600

    adding dynamic data

---
+++ src/_data/news.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+require("dotenv").config();
+
+
+module.exports = async function() {
+    try {
+      const response = await axios.get(`http://newsapi.org/v2/everything?q=bitcoin&from=2020-04-19&sortBy=publishedAt&apiKey=${process.env.API_KEY}`);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
